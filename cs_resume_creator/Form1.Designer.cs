5214ac916741d903fb43620e96210f5f9b885793
User <User@DESKTOP-172SKSR>
2022-02-19 02:49:03 +0800

    Add project files.

---
+++ cs_resume_creator/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿
+namespace cs_resume_creator
+{
+    partial class resume
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fullname_textbox = new System.Windows.Forms.TextBox();
+            this.fullname_label = new System.Windows.Forms.Label();
+            this.mobile_label = new System.Windows.Forms.Label();
+            this.mobile_textbox = new System.Windows.Forms.TextBox();
+            this.email_label = new System.Windows.Forms.Label();
+            this.email_richbox = new System.Windows.Forms.TextBox();
+            this.address_richbox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pdf_resume = new System.Windows.Forms.Button();
+            this.car_obj_label = new System.Windows.Forms.Label();
+            this.car_obj_richbox = new System.Windows.Forms.RichTextBox();
+            this.prof_att_label = new System.Windows.Forms.Label();
+            this.prof_att_richbox = new System.Windows.Forms.RichTextBox();
+            this.educ_label = new System.Windows.Forms.Label();
+            this.educ_richbox = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.extra_act_ricbox = new System.Windows.Forms.RichTextBox();
+            this.cert_etc_label = new System.Windows.Forms.Label();
+            this.cert_etc_richbox = new System.Windows.Forms.RichTextBox();
+            this.read_json = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fullname_textbox
+            // 
+            this.fullname_textbox.Enabled = false;
+            this.fullname_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fullname_textbox.Location = new System.Drawing.Point(173, 86);
+            this.fullname_textbox.Name = "fullname_textbox";
+            this.fullname_textbox.Size = new System.Drawing.Size(159, 22);
+            this.fullname_textbox.TabIndex = 0;
+            // 
+            // fullname_label
+            // 
+            this.fullname_label.AutoSize = true;
+            this.fullname_label.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fullname_label.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.fullname_label.Location = new System.Drawing.Point(80, 86);
+            this.fullname_label.Name = "fullname_label";
+            this.fullname_label.Size = new System.Drawing.Size(75, 19);
+            this.fullname_label.TabIndex = 1;
+            this.fullname_label.Text = "Fullname:";
+            // 
+            // mobile_label
+            // 
+            this.mobile_label.AutoSize = true;
+            this.mobile_label.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mobile_label.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.mobile_label.Location = new System.Drawing.Point(69, 151);
+            this.mobile_label.Name = "mobile_label";
+            this.mobile_label.Size = new System.Drawing.Size(90, 19);
+            this.mobile_label.TabIndex = 6;
+            this.mobile_label.Text = "Mobile No: ";
+            // 
+            // mobile_textbox
+            // 
+            this.mobile_textbox.Enabled = false;
+            this.mobile_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mobile_textbox.Location = new System.Drawing.Point(173, 151);
+            this.mobile_textbox.Name = "mobile_textbox";
+            this.mobile_textbox.Size = new System.Drawing.Size(100, 22);
+            this.mobile_textbox.TabIndex = 7;
+            // 
+            // email_label
+            // 
+            this.email_label.AutoSize = true;
+            this.email_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.email_label.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.email_label.Location = new System.Drawing.Point(109, 118);
+            this.email_label.Name = "email_label";
+            this.email_label.Size = new System.Drawing.Size(58, 20);
+            this.email_label.TabIndex = 2;
+            this.email_label.Text = "Email:";
+            // 
+            // email_richbox
+            // 
+            this.email_richbox.Enabled = false;
+            this.email_richbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.email_richbox.Location = new System.Drawing.Point(173, 118);
+            this.email_richbox.Name = "email_richbox";
+            this.email_richbox.Size = new System.Drawing.Size(169, 22);
+            this.email_richbox.TabIndex = 8;
+            // 
+            // address_richbox
+            // 
+            this.address_richbox.Enabled = false;
+            this.address_richbox.Location = new System.Drawing.Point(166, 196);
+            this.address_richbox.Name = "address_richbox";
+            this.address_richbox.Size = new System.Drawing.Size(208, 98);
+            this.address_richbox.TabIndex = 9;
+            this.address_richbox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(80, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 19);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Address:";
+            // 
+            // pdf_resume
+            // 
+            this.pdf_resume.BackColor = System.Drawing.Color.SteelBlue;
+            this.pdf_resume.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.pdf_resume.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pdf_resume.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pdf_resume.Location = new System.Drawing.Point(400, 455);
+            this.pdf_resume.Name = "pdf_resume";
+            this.pdf_resume.Size = new System.Drawing.Size(179, 61);
+            this.pdf_resume.TabIndex = 11;
+            this.pdf_resume.Text = "Create PDF";
+            this.pdf_resume.UseVisualStyleBackColor = false;
+            this.pdf_resume.Click += new System.EventHandler(this.pdf_resume_Click);
+            // 
+            // car_obj_label
+            // 
+            this.car_obj_label.AutoSize = true;
+            this.car_obj_label.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.car_obj_label.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.car_obj_label.Location = new System.Drawing.Point(20, 314);
+            this.car_obj_label.Name = "car_obj_label";
+            this.car_obj_label.Size = new System.Drawing.Size(129, 19);
+            this.car_obj_label.TabIndex = 12;
+            this.car_obj_label.Text = "Career Objective:";
+            // 
+            // car_obj_richbox
+            // 
+            this.car_obj_richbox.Enabled = false;
+            this.car_obj_richbox.Location = new System.Drawing.Point(166, 316);
+            this.car_obj_richbox.Name = "car_obj_richbox";
+            this.car_obj_richbox.Size = new System.Drawing.Size(208, 98);
+            this.car_obj_richbox.TabIndex = 9;
+            this.car_obj_richbox.Text = "";
+            // 
+            // prof_att_label
+            // 
+            this.prof_att_label.AutoSize = true;
+            this.prof_att_label.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.prof_att_label.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.prof_att_label.Location = new System.Drawing.Point(387, 86);
+            this.prof_att_label.Name = "prof_att_label";
+            this.prof_att_label.Size = new System.Drawing.Size(166, 19);
+            this.prof_att_label.TabIndex = 13;
+            this.prof_att_label.Text = "Professional Attributes:";
+            // 
+            // prof_att_richbox
+            // 
+            this.prof_att_richbox.Enabled = false;
+            this.prof_att_richbox.Location = new System.Drawing.Point(400, 256);
+            this.prof_att_richbox.Name = "prof_att_richbox";
+            this.prof_att_richbox.Size = new System.Drawing.Size(339, 98);
+            this.prof_att_richbox.TabIndex = 14;
+            this.prof_att_richbox.Text = "";
+            // 
+            // educ_label
+            // 
+            this.educ_label.AutoSize = true;
+            this.educ_label.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.educ_label.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.educ_label.Location = new System.Drawing.Point(762, 86);
+            this.educ_label.Name = "educ_label";
+            this.educ_label.Size = new System.Drawing.Size(80, 19);
+            this.educ_label.TabIndex = 10;
+            this.educ_label.Text = "Education:";
+            // 
+            // educ_richbox
+            // 
+            this.educ_richbox.Enabled = false;
+            this.educ_richbox.Location = new System.Drawing.Point(766, 109);
+            this.educ_richbox.Name = "educ_richbox";
+            this.educ_richbox.Size = new System.Drawing.Size(208, 98);
+            this.educ_richbox.TabIndex = 9;
+            this.educ_richbox.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(396, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 19);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Extracurricular Activitiy:";
+            // 
+            // extra_act_ricbox
+            // 
+            this.extra_act_ricbox.Enabled = false;
+            this.extra_act_ricbox.Location = new System.Drawing.Point(400, 109);
+            this.extra_act_ricbox.Name = "extra_act_ricbox";
+            this.extra_act_ricbox.Size = new System.Drawing.Size(339, 98);
+            this.extra_act_ricbox.TabIndex = 15;
+            this.extra_act_ricbox.Text = "";
+            // 
+            // cert_etc_label
+            // 
+            this.cert_etc_label.AutoSize = true;
+            this.cert_etc_label.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cert_etc_label.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.cert_etc_label.Location = new System.Drawing.Point(762, 245);
+            this.cert_etc_label.Name = "cert_etc_label";
+            this.cert_etc_label.Size = new System.Drawing.Size(330, 19);
+            this.cert_etc_label.TabIndex = 16;
+            this.cert_etc_label.Text = "Certification, Workshops, and Seminar Attended";
+            // 
+            // cert_etc_richbox
+            // 
+            this.cert_etc_richbox.Enabled = false;
+            this.cert_etc_richbox.Location = new System.Drawing.Point(757, 268);
+            this.cert_etc_richbox.Name = "cert_etc_richbox";
+            this.cert_etc_richbox.Size = new System.Drawing.Size(439, 98);
+            this.cert_etc_richbox.TabIndex = 17;
+            this.cert_etc_richbox.Text = "";
+            // 
+            // read_json
+            // 
+            this.read_json.BackColor = System.Drawing.Color.SteelBlue;
+            this.read_json.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.read_json.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.read_json.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.read_json.Location = new System.Drawing.Point(166, 455);
+            this.read_json.Name = "read_json";
+            this.read_json.Size = new System.Drawing.Size(176, 61);
+            this.read_json.TabIndex = 18;
+            this.read_json.Text = "Read JSON";
+            this.read_json.UseVisualStyleBackColor = false;
+            this.read_json.Click += new System.EventHandler(this.read_json_Click);
+            // 
+            // resume
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(1223, 545);
+            this.Controls.Add(this.read_json);
+            this.Controls.Add(this.cert_etc_richbox);
+            this.Controls.Add(this.cert_etc_label);
+            this.Controls.Add(this.extra_act_ricbox);
+            this.Controls.Add(this.prof_att_richbox);
+            this.Controls.Add(this.prof_att_label);
+            this.Controls.Add(this.car_obj_label);
+            this.Controls.Add(this.pdf_resume);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.educ_label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.car_obj_richbox);
+            this.Controls.Add(this.educ_richbox);
+            this.Controls.Add(this.address_richbox);
+            this.Controls.Add(this.email_richbox);
+            this.Controls.Add(this.mobile_textbox);
+            this.Controls.Add(this.mobile_label);
+            this.Controls.Add(this.email_label);
+            this.Controls.Add(this.fullname_textbox);
+            this.Controls.Add(this.fullname_label);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "resume";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PDF resume creator";
+            this.Load += new System.EventHandler(this.resume_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fullname_textbox;
+        private System.Windows.Forms.Label fullname_label;
+        private System.Windows.Forms.Label mobile_label;
+        private System.Windows.Forms.TextBox mobile_textbox;
+        private System.Windows.Forms.Label email_label;
+        private System.Windows.Forms.TextBox email_richbox;
+        private System.Windows.Forms.RichTextBox address_richbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button pdf_resume;
+        private System.Windows.Forms.Label car_obj_label;
+        private System.Windows.Forms.RichTextBox car_obj_richbox;
+        private System.Windows.Forms.Label prof_att_label;
+        private System.Windows.Forms.RichTextBox prof_att_richbox;
+        private System.Windows.Forms.Label educ_label;
+        private System.Windows.Forms.RichTextBox educ_richbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox extra_act_ricbox;
+        private System.Windows.Forms.Label cert_etc_label;
+        private System.Windows.Forms.RichTextBox cert_etc_richbox;
+        private System.Windows.Forms.Button read_json;
+    }
+}
+
---
+++ cs_resume_creator/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.draw;
+using iTextSharp.text;
+
+
+namespace cs_resume_creator
+{
+    public partial class resume : Form
+    {
+        
+        public resume()
+        {
+            InitializeComponent();
+        }
+
+        private void resume_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pdf_resume_Click(object sender, EventArgs e)
+        {
+            // WRITE PDF
+            Document PDFresume = new Document();
+            PdfWriter.GetInstance(PDFresume, new FileStream(@"C:\Users\User\Downloads\pdf-resume-creator-main\cs_resume_creator\cs_resume_creator\SUNGA_STEVEN_JOHN.pdf", FileMode.Create));
+            LineSeparator hl = new LineSeparator(3f, 100f, BaseColor.DARK_GRAY, Element.ALIGN_CENTER, 1);
+
+            Paragraph fname = new Paragraph(fullname_textbox.Text);
+            Paragraph add = new Paragraph(address_richbox.Text);
+            Paragraph emailMobile = new Paragraph(email_richbox.Text+" | Mobile No: "+mobile_textbox.Text+"\n\n");
+            Paragraph carObj = new Paragraph("Career Objective"+"\n"+"     " + car_obj_richbox.Text+"\n\n");
+            Paragraph profAtt = new Paragraph("Proffesional Atributes"+"\n"+"     " + prof_att_richbox.Text+"\n\n");
+            Paragraph tert = new Paragraph("Education"+"\n\n"+"Tertiary: "+educ_richbox.Text+ "\n\n");
+            Paragraph extAct = new Paragraph("Extraculicular Activity" + "\n" + "     " + extra_act_ricbox.Text + "\n\n");
+            Paragraph cert = new Paragraph("Certificate, Workshops, and Seminar Attended" + "\n" + "     " + cert_etc_richbox.Text + "\n\n\n");
+
+            PDFresume.Open();
+            PDFresume.Add(fname);
+            PDFresume.Add(add);
+            PDFresume.Add(emailMobile);
+            PDFresume.Add(hl);
+            PDFresume.Add(carObj);
+            PDFresume.Add(profAtt);
+            PDFresume.Add(tert);
+            PDFresume.Add(extAct);
+            PDFresume.Add(cert);
+            PDFresume.Close();
+
+            MessageBox.Show("PDF file create successfully", "Information" ,MessageBoxButtons.OK);
+        }
+
+        public class Readjson
+        {
+            public string fullname { get; set; }
+            public string address { get; set; }
+            public string email { get; set; }
+            public string mobile { get; set; }
+            public string car_obj { get; set; }
+            public string prof_att { get; set; }
+            public string tertiary { get; set; }
+            public string extra_act { get; set; }
+            public string cert { get; set; }
+        }
+
+        private void read_json_Click(object sender, EventArgs e)
+        {
+            // READ JSON
+            string jsonFile = @"C:\Users\User\Downloads\pdf-resume-creator-main\cs_resume_creator\cs_resume_creator\resume_info.json";
+            string readJsonFile = File.ReadAllText(jsonFile);
+            Readjson jsonFileOutput = JsonConvert.DeserializeObject<Readjson>(readJsonFile);
+
+            fullname_textbox.Text = jsonFileOutput.fullname;
+            address_richbox.Text = jsonFileOutput.address;
+            email_richbox.Text = jsonFileOutput.email;
+            mobile_textbox.Text = jsonFileOutput.mobile;
+            car_obj_richbox.Text = jsonFileOutput.car_obj;
+            prof_att_richbox.Text = jsonFileOutput.prof_att;
+            educ_richbox.Text = jsonFileOutput.tertiary;
+            extra_act_ricbox.Text = jsonFileOutput.extra_act;
+            cert_etc_richbox.Text = jsonFileOutput.cert;
+
+            fullname_textbox.Enabled = true;
+            address_richbox.Enabled = true;
+            email_richbox.Enabled = true;
+            mobile_textbox.Enabled = true;
+            car_obj_richbox.Enabled = true;
+            prof_att_richbox.Enabled = true;
+            educ_richbox.Enabled = true;
+            extra_act_ricbox.Enabled = true;
+            cert_etc_richbox.Enabled = true;
+        }
+    }
+}
